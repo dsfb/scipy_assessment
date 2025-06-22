6ea8f53d60eebb98e4b1686edb10f4f2c8ff2049
Daniel Bruno <dsfbstudent@yahoo.com.br>
2025-06-22 20:08:30 -0300

    Fix test definition from two commits ago.

--- scipy/sparse/tests/test_matrix.py
+++ scipy/sparse/tests/test_matrix.py
@@ -5,7 +5,7 @@ from numpy.testing import assert_array_almost_equal, assert_, assert_raises
 from scipy.sparse import csr_matrix
 
 
-def _check_round_csr_matrix():
+def test_check_round_csr_matrix():
     a = csr_matrix((3, 4))
     assert_raises(AssertionError, assert_raises, TypeError, round(a))
     assert_(assert_raises(TypeError, round(a))  is None)
