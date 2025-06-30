8ed9b9625418be684e38398c7c5e66434a076ad6
Daniel Bruno <dsfbstudent@yahoo.com.br>
2025-06-30 08:53:53 -0300

    Addendum for the last commit.

--- scipy/sparse/tests/test_matrix.py
+++ scipy/sparse/tests/test_matrix.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_equal, assert_, assert_raises
-from scipy.sparse import csr_matrix
+from scipy.sparse import base, csr_matrix
 
 
 def test_check_round_csr_matrix():
@@ -12,4 +12,4 @@ def test_check_round_csr_matrix():
 
 def test_check_round_csr_matrix2():
      A = csr_matrix((5, 7))
-     assert_equal(round(A), round(self.spmatrix(A)).todense())
+     assert_equal(round(A), round(spmatrix(A)).todense())
