336adf12c05b01ea17867064161659f86e096e8e
Daniel Bruno <dsfbstudent@yahoo.com.br>
2025-06-30 10:50:44 -0300

    Improve the tests again...

--- scipy/sparse/tests/test_matrix.py
+++ scipy/sparse/tests/test_matrix.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_almost_equal, assert_equal, assert_, assert_raises
-from scipy.sparse import base, csr_matrix
+from scipy.sparse import base, csr_matrix, eye
 
 
 def test_check_round_csr_matrix():
@@ -12,5 +12,5 @@ def test_check_round_csr_matrix():
 
 def test_check_round_csr_matrix2():
      A = csr_matrix((5, 7))
-     assert_almost_equal(round(A), round(csr_matrix(A).todense()))
-     assert_equal(round(A), round(csr_matrix(A).todense()))
+     B = eye(3)
+     assert_equal(round(B), 3)
